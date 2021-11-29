11467985495a68aada2878592d2e412ffaa61020
sal0ni <mahatmasaloni@gmail.com>
2021-11-28 20:19:51 -0500

    Update test_csv_reader.py

--- test/test_csv_reader.py
+++ test/test_csv_reader.py
@@ -4,17 +4,20 @@ sys.path.append(os.path.abspath('../../'))
 
 from Recipe_Recommender.Data.csv_reader import restaurant_data, location_data, diet_type_data 
 
-def test_restaurant_data():
+def test_restaurant_data_punjabi():
     assert "Punjabi Dhaba" in restaurant_data("Punjabi", "North Indian", "Punjabi Dhaba", "")
+    
+def test_restaurant_data_indian():
+    assert "Gujarati" in restaurant_data("Indian", "Gujarati", "Indian", "")
     
 def test_location_data():
     assert "Raleigh" in location_data("Punjabi", "North Indian", "Raleigh", "")
 
 def test_diet_type_data_Non_Veg():
-		assert "Non-Vegetarian" in diet_type_data("chicken")
+	assert "Non-Vegetarian" in diet_type_data("chicken")
 
 def test_diet_type_data_Veg():
-		assert "Vegetarian" in diet_type_data("milk")
+	assert "Vegetarian" in diet_type_data("milk")
 
 def test_diet_type_data_Vegan():
-		assert "Vegan" in diet_type_data("oats")
+	assert "Vegan" in diet_type_data("oats")
